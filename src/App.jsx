9939c878295816196cd85e9d209b3911b5fa6684
Sami S. <91660927+azukw@users.noreply.github.com>
2025-12-30 23:05:20 +0100

    fixes for google

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { GameProvider } from "./context/GameContext";
 import Game from "./pages/Game";
+import { Helmet } from "react-helmet-async";
 import "./App.css";
 
 function App() {
     return (
         <GameProvider>
+
+            <Helmet>
+                <title>Mindster – Jeu de logique de combinaison de couleurs</title>
+                <meta
+                    name="description"
+                    content="Mindster est un jeu de réflexion où vous devez trouver la bonne combinaison de couleurs en 8 essais. Simple, rapide et addictif."
+                />
+                <link rel="canonical" href="https://mindster.fr" />
+            </Helmet>
+
             <Game />
+
         </GameProvider>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <HelmetProvider>
+        <App />
+    </HelmetProvider>
+);
